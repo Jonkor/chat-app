9baf2d0d250847783292585aac9d8a664d992cda
Jonkor <juan.kucho@hotmail.com>
2025-06-21 23:09:38 -0700

    refactored removing some coment lines

--- src/index.js
+++ src/index.js
@@ -15,7 +15,6 @@ const port = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const publicDirectoryPath = path.join(__dirname, '../public');
-// const { addUser, removeUser, getUser, getUsersInRoom } = from ('./utils/users');
 
 app.use(express.static(publicDirectoryPath));
 
